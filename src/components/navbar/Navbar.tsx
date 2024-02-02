aeeaa8fb207c72416c31bc7306e67a08c7aeaec6
andreastif <45339645+andreastif@users.noreply.github.com>
2024-02-02 10:42:36 +0100

    added logo

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,25 +1,34 @@
 import "./Navbar.css";
+import logo from "../../img/recipe-bro.png";
 
 function Navbar() {
-  return (
-    <>
-      <div className="bg-img">
-        <div className="container">
-          <div className="topnav rounded p-4">
-            <a className="h1" href="#">
-              <span className="h4">Login</span>
-            </a>
-            <a href="#">
-              <span className="h4">Register</span>
-            </a>
-            <a href="#">
-              <span className="h4">About</span>
-            </a>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div className="bg-img">
+				<div className="container">
+					<div className="topnav rounded p-4">
+						<a href="#">
+							<span className="h4">Login</span>
+						</a>
+						<a href="#">
+							<span className="h4">Register</span>
+						</a>
+						<a href="#">
+							<span className="h4">About</span>
+						</a>
+						<div className="">
+							<img
+								className="img-fluid"
+								width={100}
+								height={100}
+								src={logo}
+							/>
+						</div>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 }
 
 export default Navbar;
