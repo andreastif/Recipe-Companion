67fa6a19c4d6e0c43f3e9c9c06c7fc22587d38be
andreastif <45339645+andreastif@users.noreply.github.com>
2024-02-02 17:27:19 +0100

    changed background wallpaper

--- src/components/navbars/LandingPageNavbar.tsx
+++ src/components/navbars/LandingPageNavbar.tsx
@@ -1,29 +1,34 @@
 import "./LandingPageNavbar.css";
 import logo from "../../img/recipe-bro.png";
 import {Link} from "react-router-dom";
+import {useAuth} from "../../hooks/useAuth.tsx";
 
 function LandingPageNavbar() {
-	return (
-		<>
-			<div className="bg-img">
-				<div className="container">
-					<div className="topnav rounded p-4">
-						<div>
-							<img
-								className="img-fluid"
-								width={100}
-								height={100}
-								src={logo}
-							/>
-						</div>
-						<Link to={"/login"}><span className="h4">Login</span></Link>
-						<Link to={"/register"}><span className="h4">Register</span></Link>
-						<Link to={"/about"}><span className="h4">About</span></Link>
-					</div>
-				</div>
-			</div>
-		</>
-	);
+    const {user} = useAuth();
+
+    return (
+        <>
+            <div className="bg-img">
+                <div className="container">
+                    <div className="topnav rounded p-4">
+                        <div>
+                            <img
+                                className="img-fluid"
+                                width={100}
+                                height={100}
+                                src={logo}
+                            />
+                        </div>
+                        { !user ? <div>
+                            <Link to={"/login"}><span className="h4">Login</span></Link>
+                            <Link to={"/register"}><span className="h4">Register</span></Link>
+                        </div> : <Link to={"/dashboard"}><span className="h4">Dashboard</span></Link>}
+                        <Link to={"/about"}><span className="h4">About</span></Link>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 }
 
 export default LandingPageNavbar;
