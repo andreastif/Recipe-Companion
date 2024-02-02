b820a59974588081c39ad86d1d4799480cd2a7d8
alsomeb <alsomeb@gmail.com>
2024-02-02 10:18:16 +0100

    - navbar media queries

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,19 +1,25 @@
-import "./Navbar.css"
+import "./Navbar.css";
 
 function Navbar() {
-    return (
-        <>
-            <div className="bg-img">
-                <div className="container">
-                    <div className="topnav rounded p-4">
-                        <a href="#">Login</a>
-                        <a href="#">Register</a>
-                        <a href="#">About</a>
-                    </div>
-                </div>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div className="bg-img">
+        <div className="container">
+          <div className="topnav rounded p-4">
+            <a className="h1" href="#">
+              <span className="h4">Login</span>
+            </a>
+            <a href="#">
+              <span className="h4">Register</span>
+            </a>
+            <a href="#">
+              <span className="h4">About</span>
+            </a>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Navbar;
