7d22066383ae7332be1d73b708e2f8669b8a2a7f
andreastif <45339645+andreastif@users.noreply.github.com>
2024-02-09 13:49:33 +0100

    fixed landingpage navbar to be of the same design as the logged in navbar, updated logout, profile

--- src/components/navbars/loggedin/LoggedInNav.tsx
+++ src/components/navbars/loggedin/LoggedInNav.tsx
@@ -1,15 +1,30 @@
 import "./LoggedInNav.css"
-import {NavLink} from "react-router-dom";
+import {NavLink, useNavigate} from "react-router-dom";
 import logo from "../../../img/test-icon.png"
+import {useAuth} from "../../../hooks/useAuth.tsx";
 
 function LoggedInNav() {
+    const {logout} = useAuth();
+
+    const navigate = useNavigate();
+
+    const handleLogout = async () => {
+        try {
+            await logout();
+            navigate("/");
+        } catch (error: any) {
+            console.log(error.message);
+        }
+    };
+
+
     // https://reactrouter.com/en/main/components/nav-link
     return (
         <div>
             <nav className="navbar navbar-expand-lg bg-body-tertiary border-bottom" data-bs-theme="dark">
                 <div className="container-fluid">
                     <div>
-                        <img src={logo} alt="logo" className="me-2 img-fluid nav-icon" />
+                        <img src={logo} alt="logo" className="me-2 img-fluid nav-icon"/>
                     </div>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
                             data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"
@@ -21,19 +36,21 @@ function LoggedInNav() {
                             <li className="nav-item">
                                 <NavLink
                                     to="/dashboard"
-                                    className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}>
+                                    className={({isActive}) => isActive ? "nav-link active" : "nav-link"}>
                                     Dashboard
                                 </NavLink>
                             </li>
                             <li className="nav-item">
                                 <NavLink
                                     to="/profile"
-                                    className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}>
+                                    className={({isActive}) => isActive ? "nav-link active" : "nav-link"}>
                                     Profile
                                 </NavLink>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="#">Pricing</a>
+                                <span className="nav-link" style={{'cursor':'pointer'}} onClick={handleLogout} >
+                                    Logout
+                                </span>
                             </li>
                         </ul>
                     </div>
