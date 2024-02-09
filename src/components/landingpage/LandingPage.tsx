16a5109487e982adf01fe150d95adb6677f0a944
alsomeb <alsomeb@gmail.com>
2024-02-09 09:47:12 +0100

    - Changed name of app

--- src/components/landingpage/LandingPage.tsx
+++ src/components/landingpage/LandingPage.tsx
@@ -6,10 +6,10 @@ function LandingPage() {
         <>
             <LandingPageNavbar/>
             <div className="landing">
-                <h1 className="mt-4 mb-4">Meet Recipe Bro</h1>
+                <h1 className="mt-4 mb-4">Meet Recipe Companion</h1>
                 <p>Your AI-powered kitchen companion! Just enter your ingredients, and it crafts personalized recipes
                     with easy-to-follow steps.</p>
-                <p>Elevate your meals effortlessly with Recipe Bro, simplifying your culinary journey with innovative
+                <p>Elevate your meals effortlessly with Recipe Companion, simplifying your culinary journey with innovative
                     and delicious results</p>
             </div>
         </>
