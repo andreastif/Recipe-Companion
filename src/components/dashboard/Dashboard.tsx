d3b42531cdd7d3321502a1f430e2272a9c67d0a3
alsomeb <alsomeb@gmail.com>
2024-02-11 15:31:50 +0100

    - Dashboard card style examples

--- src/components/dashboard/Dashboard.tsx
+++ src/components/dashboard/Dashboard.tsx
@@ -5,6 +5,7 @@ import Box from '@mui/material/Box';
 
 import LoggedInNav from "../navbars/loggedin/LoggedInNav.tsx";
 import {SyntheticEvent, useState} from "react";
+import {Card, CardActionArea, CardContent, CardMedia, Typography} from "@mui/material";
 
 function Dashboard() {
     const [value, setValue] = useState('one');
@@ -36,25 +37,123 @@ function Dashboard() {
                                 </Tabs>
                             </Box>
                         </div>
-                        <div className="p-4 ">"But I must explain to you how all this mistaken idea of
-                            denouncing pleasure and praising pain was born and I will give you a complete account of the
-                            system, and expound the actual teachings of the great explorer of the truth, the
-                            master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself,
-                            because it is pleasure, but because those who do not know how to pursue pleasure rationally
-                            encounter consequences that are extremely painful. Nor again is there anyone who loves or
-                            pursues or desires to obtain pain of itself, because it is pain, but because occasionally
-                            circumstances occur in which toil and pain can procure him some great pleasure. To take a
-                            trivial example, which of us ever undertakes laborious physical exercise, except to obtain
-                            some advantage from it? But who has any right to find fault with a man who chooses to enjoy
-                            a pleasure that has no annoying consequences, or one who avoids a pain that produces no
-                            resultant pleasure?"
+                        <div className="p-5 row">
+                            <div className="col-sm-12 col-md-6 col-lg-4 col-xl-3 mb-5">
+                                <Card sx={{ maxWidth: 345, bgcolor: '#2b3035', border: '1px solid white'}}>
+                                    <CardActionArea>
+                                        <CardMedia
+                                            component="img"
+                                            height="140"
+                                            src="https://picsum.photos/140"
+                                            alt="lorem picsum"
+                                        />
+                                        <CardContent>
+                                            <Typography gutterBottom variant="h5" component="div"
+                                                        className="text-white text-center">
+                                                Lizard
+                                            </Typography>
+                                            <Typography variant="body2" color="white">
+                                                Lizards are a widespread group of squamate reptiles, with over 6,000
+                                                species, ranging across all continents except Antarctica
+                                            </Typography>
+                                        </CardContent>
+                                    </CardActionArea>
+                                </Card>
+                            </div>
+                            <div className="col-sm-12 col-md-6 col-lg-4 col-xl-3 mb-5">
+                                <Card sx={{maxWidth: 345}}>
+                                    <CardActionArea>
+                                        <CardMedia
+                                            component="img"
+                                            height="140"
+                                            src="https://picsum.photos/140"
+                                            alt="lorem picsum"
+                                        />
+                                        <CardContent>
+                                            <Typography gutterBottom variant="h5" component="div"
+                                                        className="text-black text-center">
+                                                Lizard
+                                            </Typography>
+                                            <Typography variant="body2" color="text.secondary">
+                                                Lizards are a widespread group of squamate reptiles, with over 6,000
+                                                species, ranging across all continents except Antarctica
+                                            </Typography>
+                                        </CardContent>
+                                    </CardActionArea>
+                                </Card>
+                            </div>
+                            <div className="col-sm-12 col-md-6 col-lg-4 col-xl-3 mb-5">
+                                <Card sx={{maxWidth: 345}}>
+                                    <CardActionArea>
+                                        <CardMedia
+                                            component="img"
+                                            height="140"
+                                            src="https://picsum.photos/140"
+                                            alt="lorem picsum"
+                                        />
+                                        <CardContent>
+                                            <Typography gutterBottom variant="h5" component="div"
+                                                        className="text-black text-center">
+                                                Lizard
+                                            </Typography>
+                                            <Typography variant="body2" color="text.secondary">
+                                                Lizards are a widespread group of squamate reptiles, with over 6,000
+                                                species, ranging across all continents except Antarctica
+                                            </Typography>
+                                        </CardContent>
+                                    </CardActionArea>
+                                </Card>
+                            </div>
+                            <div className="col-sm-12 col-md-6 col-lg-4 col-xl-3 mb-5">
+                                <Card sx={{maxWidth: 345}}>
+                                    <CardActionArea>
+                                        <CardMedia
+                                            component="img"
+                                            height="140"
+                                            src="https://picsum.photos/140"
+                                            alt="lorem picsum"
+                                        />
+                                        <CardContent>
+                                            <Typography gutterBottom variant="h5" component="div"
+                                                        className="text-black text-center">
+                                                Lizard
+                                            </Typography>
+                                            <Typography variant="body2" color="text.secondary">
+                                                Lizards are a widespread group of squamate reptiles, with over 6,000
+                                                species, ranging across all continents except Antarctica
+                                            </Typography>
+                                        </CardContent>
+                                    </CardActionArea>
+                                </Card>
+                            </div>
+                            <div className="col-sm-12 col-md-6 col-lg-4 col-xl-3 mb-5">
+                                <Card sx={{maxWidth: 345}}>
+                                    <CardActionArea>
+                                        <CardMedia
+                                            component="img"
+                                            height="140"
+                                            src="https://picsum.photos/140"
+                                            alt="lorem picsum"
+                                        />
+                                        <CardContent>
+                                            <Typography gutterBottom variant="h5" component="div"
+                                                        className="text-black text-center">
+                                                Lizard
+                                            </Typography>
+                                            <Typography variant="body2" color="text.secondary">
+                                                Lizards are a widespread group of squamate reptiles, with over 6,000
+                                                species, ranging across all continents except Antarctica
+                                            </Typography>
+                                        </CardContent>
+                                    </CardActionArea>
+                                </Card>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </>
-    )
-        ;
+    );
 }
 
 export default Dashboard;
