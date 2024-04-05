dc1ffd4eb5ed3ce42d1207e554b137d20a28e6d7
alsomeb <alsomeb@gmail.com>
2024-04-05 16:32:19 +0200

    - Login.tsx small text change

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -103,7 +103,7 @@ function Login() {
                                    value={form.email}
                                    onChange={handleFormControlChange}
                             />
-                            <div id="emailHelp" className="form-text">We'll never share your email with anyone else.
+                            <div id="emailHelp" className="form-text text-sm-center mt-3">We'll never share your email with anyone else.
                             </div>
                         </div>
                         <div className="mb-3">
