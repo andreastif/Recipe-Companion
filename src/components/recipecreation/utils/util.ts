7b0134e01343cc0ccebc5c3418be02fa5e2c4d1d
alsomeb <alsomeb@gmail.com>
2024-05-14 12:34:31 +0200

    - Changed heights

--- src/components/recipecreation/utils/util.ts
+++ src/components/recipecreation/utils/util.ts
@@ -1,4 +1,4 @@
 export const getRandomHeight = () => {
-  const heights = [180, 200, 250, 300, 350, 400];
+  const heights = [180, 200, 250];
   return heights[Math.floor(Math.random() * heights.length)];
 };
