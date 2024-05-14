6377584b91026cd5980cb1393d99ffc4810f3901
andreastif <45339645+andreastif@users.noreply.github.com>
2024-05-14 11:47:43 +0200

    added scrollable container

--- src/components/inspirationtab/muiStyles.ts
+++ src/components/inspirationtab/muiStyles.ts
@@ -1,32 +1,34 @@
 export const modalBoxStyle = (isMobile: boolean) => {
-    return {
-        position: 'absolute',
-        top: '50%',
-        left: '50%',
-        transform: 'translate(-50%, -50%)',
-        width: isMobile ? '90%' : '40%',
-        bgcolor: '#2b3035',
-        border: '1px solid #FFF',
-        borderRadius: '5px',
-        boxShadow: 24,
-        p: 4,
-    }
+  return {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: isMobile ? "90%" : "40%",
+    bgcolor: "#2b3035",
+    border: "1px solid #FFF",
+    borderRadius: "5px",
+    boxShadow: 24,
+    p: 4,
+    overflowY: "auto",
+    maxHeight: "90vh",
+  };
 };
 
 export const inspoContainer = (isMobile: boolean) => {
-    return {
-        width: isMobile ? "100%" : "95%",
-        height: "90%",
-        paddingTop: isMobile ? "5px" : "40px",
-        paddingBottom: isMobile ? "0px" : "20px"
-    }
+  return {
+    width: isMobile ? "100%" : "95%",
+    height: "90%",
+    paddingTop: isMobile ? "5px" : "40px",
+    paddingBottom: isMobile ? "0px" : "20px",
+  };
 };
 
 export const recipeButtonStyle = () => {
-    return {
-        bgcolor: '#895c09',
-        '&:hover': {
-            bgcolor: 'rgba(137,92,9,0.65)'
-        }
-    }
-}
+  return {
+    bgcolor: "#895c09",
+    "&:hover": {
+      bgcolor: "rgba(137,92,9,0.65)",
+    },
+  };
+};
